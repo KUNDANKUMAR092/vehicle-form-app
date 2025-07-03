db1ef9e12725722027815cf3b0ee1c35615c47a9
kundan singh <kundan.singh092@gmail.com>
2025-07-03 19:57:16 +0530

    Add files via upload
    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import VehicleForm from './VehicleForm';
+
+function App() {
+  return (
+    <div className="App">
+      <VehicleForm />
+    </div>
+  );
+}
+
+export default App;
